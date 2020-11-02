b8d4cc83ce6ecb779b43147eadfe2c394033ee8e
Debra Wolford <debra.wolford@gmail.com>
2020-11-01 20:18:36 -0600

    created product details page

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def products(request):
-    """ Renders all products ability to search and sort"""
+    """ Renders all products and ability to search and sort"""
 
     products = Product.objects.all()
 
@@ -13,3 +13,14 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_details(request, product_id):
+    """ Renders product details on a specific product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
