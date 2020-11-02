38c5c0b3557451964d722e42b49e8a222f0a5959
Debra Wolford <debra.wolford@gmail.com>
2020-11-02 15:55:02 -0600

    formatted shopping cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,24 +1,38 @@
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
 
     cart_items = []
-    order_total = 0
+    total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
 
-    if order_total < settings.FREE_SHIPPING_THRESHOLD:
-        shipping = settings.STANDARD_SHIPPING_COST
-        free_shipping_delta = settings.FREE_SHIPPING_THRESHOLD - order_total
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total < settings.FREE_SHIPPING_THRESHOLD:
+        shipping = total * Decimal(settings.STANDARD_SHIPPING_COST / 100)
+        free_shipping_delta = settings.FREE_SHIPPING_THRESHOLD - total
     else:
         shipping = 0
         free_shipping_delta = 0
 
-    grand_total = shipping + order_total
+    grand_total = shipping + total
 
     context = {
         'cart_items': cart_items,
-        'order_total': order_total,
+        'total': total,
         'product_count': product_count,
         'shipping': shipping,
         'free_shipping_delta': free_shipping_delta,
