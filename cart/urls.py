bd14d7b78882a49ca532ec893d4cf2c0d7bbe6d5
Debra Wolford <debra.wolford@gmail.com>
2020-11-03 20:01:30 -0600

    Added alerts

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
-    path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
-    path('remove/<item_id>', views.remove_from_cart, name='remove_from_cart'),
-    path('update/<item_id>', views.update_quantity, name='update_quantity'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update/<item_id>/', views.update_quantity, name='update_quantity'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 # Views
 
 
@@ -11,14 +14,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Adds specified quantity of product to cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Added {quantity} for a total of {cart[item_id]} {product.name}.')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {quantity} {product.name}(s) to cart.')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -28,18 +34,23 @@ def remove_from_cart(request, item_id):
     """
     Remove product from cart
     """
+    product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
     cart.pop(item_id)
+    messages.success(request, f'Removed {product.name} from cart.')
     request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+    return redirect(redirect_url)
 
 
 def update_quantity(request, item_id):
     """
     Updates the quantity of the specified product
     """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
     cart[item_id] = quantity
+    messages.success(request, f'Added {quantity} for a total of {cart[item_id]} {product.name}.')
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
