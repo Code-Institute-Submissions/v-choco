db7e2888dee919a31aef92d784635ce35d1883ff
Debra Wolford <debra.wolford@gmail.com>
2020-11-12 16:39:43 -0600

    Added order history on profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
+
 def profile(request):
     """ Displays user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -13,6 +24,25 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'profile_page': True,
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        'This is a summary of an order you placed in the past'
+        f'The order number is {order_number}.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
