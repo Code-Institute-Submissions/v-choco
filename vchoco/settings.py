a90a61c4f96f1868a101558d117152124a26be7c
Debra Wolford <debra.wolford@gmail.com>
2020-11-19 16:30:29 -0600

    prepared for deployment to heroku

--- vchoco/settings.py
+++ vchoco/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'dda809cb0bed.ngrok.io',
+    'v-choco.herokuapp.com'
 ]
 
 
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'vchoco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
