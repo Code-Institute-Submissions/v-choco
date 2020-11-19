934c3d380d7a4ed1ce2af1445728b81f8ea55d15
Debra Wolford <debra.wolford@gmail.com>
2020-11-19 16:37:08 -0600

    Updated secret key and debug

--- vchoco/settings.py
+++ vchoco/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fd(qnn_s8br@04dc&k-cl_t34kt!ewdf$*%pfsnt&fl#u%-w=b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
