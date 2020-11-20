c5a6f1cee5e08d99f2d2b966779cafc9acd0814d
Debra Wolford <debra.wolford@gmail.com>
2020-11-19 20:30:08 -0600

    added path to AWS settings

--- vchoco/settings.py
+++ vchoco/settings.py
@@ -184,6 +184,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket configuration
+    AWS_S3_ADDRESSING_STYLE = "path"
     AWS_STORAGE_BUCKET_NAME = 'v-choco'
     AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
